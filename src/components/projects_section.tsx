c766c30601ffb0443482f207ebde2b70ad7dea72
Alex Holly <hollya@oregonstate.edu>
2024-08-18 17:09:56 -0700

    adding video link

--- src/components/projects_section.tsx
+++ src/components/projects_section.tsx
@@ -8,7 +8,7 @@ const Projects: React.FC = () => {
             <h1>Projects</h1>
             <h2>I believe one of the best ways to expand and test my knowledge is through through projects. Here are a some of the recent projects I have been working on.</h2>
             <div className="container">
-                <Project url="https://www.youtube.com/watch?v=N3tRFayqVtk&list=PLPtbyHU0DmMmqffxtnHMpGxXKwdiAJqGU&index=3" text="Simple Creature Nueral Net Evolution Simulation"/>
+                <Project url="https://youtu.be/wBHxsVFhX0U" text="AlexOS C programming"/>
                 <Project url="https://www.youtube.com/watch?v=Bfcg4tS8hpw" text="Ecosystem Evolution Simulation"/>
                 <WebsiteProject text="Random Website" url="https://www.godaddy.com" thumbnailUrl="https://via.placeholder.com/320x180"/>
             </div>
