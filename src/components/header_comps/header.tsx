a8e8817d1f6c988940e6ff2a714a7c209c055692
Alex Holly <alho@uoregon.edu>
2025-11-14 21:05:14 -0800

    adding sim project

--- src/components/header_comps/header.tsx
+++ src/components/header_comps/header.tsx
@@ -21,7 +21,9 @@ const Header: React.FC = () => {
             </Header_link>
             
             <div className="icon_class">
-                <a href={"https://docs.google.com/document/d/1LZ5sCFjnq4bRzHqrio42gTC8AWEDUmu9/edit?usp=sharing&ouid=116882288099539112958&rtpof=true&sd=true"} className="linkid" target="_blank" rel="noopener noreferrer">Resume</a>
+                <a href="/Portfolio/AlexHolly_resume.docx" className="linkid" target="_blank" rel="noopener noreferrer">
+                    Resume
+                </a>
                 <Spacer width="25px" />
                 <Icon site_link="https://www.linkedin.com/in/alex-holly213/" icon_img={faLinkedin} size='1x'/>
                 <Spacer width="25px" />
